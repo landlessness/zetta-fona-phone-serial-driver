d0529d4406b33b8e3210aea4b10c31396ec26266
landlessness <brian@landlessness.net>
2014-11-10 12:24:05 -0500

    full feature support

--- fona_phone.js
+++ fona_phone.js
@@ -4,6 +4,16 @@ var util = require('util');
 var FonaPhone = module.exports = function() {
   Device.call(this);
   this._serialDevice = arguments[0];
+  
+  this.volume = null;
+  this.audioModeCode = null;
+  this.audioMode = null;
+  
+  this._audioModesMap = {
+    0: 'headset',
+    1: 'external'
+  };
+
 };
 util.inherits(FonaPhone, Device);
 
@@ -13,14 +23,58 @@ FonaPhone.prototype.init = function(config) {
   .name('Adafruit Fona Phone')
   .type('fona-phone')
   .state('waiting')
-  .when('waiting', { allow: ['call', 'pick-up']})
+  .when('waiting', { allow: ['call', 'pick-up', 'set-volume', 'get-volume', 'get-audio-mode', 'set-audio-mode', 'play-toolkit-tone', 'set-mic-mode-level']})
+  .when('playing-toolkit-tone', {allow: ['stop-toolkit-tone']})
   .when('calling', { allow: ['hang-up']})
   .when('ringing', { allow: ['pick-up', 'hang-up']})
-  .when('talking', { allow: ['hang-up']})
-  .map('hang-up', this.hangUp, [])
-  .map('pick-up', this.pickUp, [])
-  .map('call', this.callPhone, [
-    { name: 'phoneNumber', title: 'Phone Number to Call', type: 'text'}])
+  .when('talking', { allow: ['hang-up', 'set-volume', 'get-volume', 'get-audio-mode', 'set-audio-mode', 'set-mic-mode-level']})
+  .map('hang-up', this.hangUp)
+  .map('pick-up', this.pickUp)
+  .map('stop-toolkit-tone', this.stopToolkitTone)
+  .map('play-toolkit-tone', this.playToolkitTone, [{
+    name: 'tone',
+    type: 'range',
+    text: 'Tone (1-8,16-20)',
+    min: 1,
+    max: 20,
+    step: 1
+    // this is really two ranges. 1-8 and 16-20.
+    // +STTONE: (0-1),(1-8,16-20),(10-15300000)
+  },{
+    name: 'length',
+    text: 'Length in Milliseconds',
+    type: 'range',
+    min: 10,
+    max: 15300000,
+    step: 1
+  }])
+  .map('set-mic-mode-level', this.setMicModeLevel, [{
+    name: 'mode', title: 'Mic Mode', type: 'radio', 
+          value: [{value: 0, text: this._audioModesMap[0]},
+                  {value: 1, text: this._audioModesMap[1]}]
+  },{
+    name: 'level', title: 'Mic Gain', type: 'range',
+    min: 0,
+    max: 15,
+    step: 1
+  }])
+  .map('set-audio-mode', this.setAudioMode, [{
+    name: 'mode', title: 'Audio Mode', type: 'radio', 
+          value: [{value: 0, text: this._audioModesMap[0]},
+                  {value: 1, text: this._audioModesMap[1]}]
+    }])
+  .map('get-audio-mode', this.getAudioMode)
+  .map('set-volume', this.setVolume, [{
+    name: 'volume',
+    type: 'range',
+    min: 0,
+    max: 100,
+    step: 1}])
+  .map('get-volume', this.getVolume)
+  .map('call', this.callPhone, [{
+    name: 'phoneNumber',
+    title: 'Phone Number to Call',
+    type: 'text'}])
   
   this._listenForIncomingCall();
   
@@ -107,3 +161,168 @@ FonaPhone.prototype.pickUp = function(taskIsDone) {
     taskIsDone();
   });
 };
+
+FonaPhone.prototype.setVolume = function(volume, taskIsDone) {
+  this.log('setVolume');
+
+  var self = this;
+  
+  var tasks = [
+  {
+    before: function() {self.state = 'setting-volume'},
+    command: 'AT+CLVL=' + volume,
+    regexp: /^$/
+  },
+  {
+    regexp: /OK/,
+    onMatch: function(match) {
+      self.state = 'waiting';
+      taskIsDone();
+    }
+  }
+  ];
+
+  this._serialDevice.enqueue(tasks);
+};
+
+FonaPhone.prototype.getVolume = function(taskIsDone) {
+  this.log('getVolume');
+
+  var self = this;
+  
+  var tasks = [
+  {
+    before: function() {self.state = 'getting-volume'},
+    command: 'AT+CLVL?',
+    regexp: /^$/
+  },
+  {
+    regexp: /\+CLVL: (\d+)/,
+    onMatch: function(match) {
+      self.volume = match[1];
+      self.state = 'waiting';
+      taskIsDone();
+    }
+  }
+  ];
+
+  this._serialDevice.enqueue(tasks);
+};
+
+FonaPhone.prototype.setAudioMode = function(mode, taskIsDone) {
+  this.log('setAudioMode');
+
+  var self = this;
+  
+  var tasks = [
+  {
+    before: function() {self.state = 'setting-audio-mode'},
+    command: 'AT+CHFA=' + mode,
+    regexp: /^$/
+  },
+  {
+    regexp: /OK/,
+    onMatch: function(match) {
+      self.state = 'waiting';
+      taskIsDone();
+    }
+  }
+  ];
+
+  this._serialDevice.enqueue(tasks);
+};
+
+FonaPhone.prototype.getAudioMode = function(taskIsDone) {
+  this.log('getAudioMode');
+
+  var self = this;
+  
+  var tasks = [
+  {
+    before: function() {self.state = 'getting-audio-mode'},
+    command: 'AT+CHFA?',
+    regexp: /^$/
+  },
+  {
+    regexp: /\+CHFA: (\d+)/,
+    onMatch: function(match) {
+      self.audioModeCode = match[1];
+      self.audioMode = self._audioModesMap[match[1]];
+      self.state = 'waiting';
+      taskIsDone();
+    }
+  }
+  ];
+
+  this._serialDevice.enqueue(tasks);
+};
+
+FonaPhone.prototype.playToolkitTone = function(tone, length, taskIsDone) {
+  this.log('playToolkitTone');
+
+  var self = this;
+  
+  var tasks = [
+  {
+    before: function() {self.state = 'playing-toolkit-tone';},
+    command: ['AT+STTONE=1',tone,length].join(','),
+    regexp: /^$/
+  },
+  {
+    regexp: /OK/,
+    onMatch: function() {
+      taskIsDone();
+      setTimeout(function(){
+        self.state = 'waiting';
+      }, length);
+    }
+  }
+  ];
+
+  this._serialDevice.enqueue(tasks);
+}
+
+FonaPhone.prototype.stopToolkitTone = function(taskIsDone) {
+  this.log('stopToolkitTone');
+
+  var self = this;
+  
+  var tasks = [
+  {
+    before: function() {self.state = 'stopping-toolkit-tone'},
+    command: 'AT+STTONE=0',
+    regexp: /^$/
+  },
+  {
+    regexp: /OK/,
+    onMatch: function(match) {
+      self.state = 'waiting';
+      taskIsDone();
+    }
+  }
+  ];
+
+  this._serialDevice.enqueue(tasks);
+}
+FonaPhone.prototype.setMicModeLevel = function(mode, level, taskIsDone) {
+  this.log('setMicModeLevel');
+
+  var self = this;
+  
+  var tasks = [
+  {
+    before: function() {self.state = 'setting-mic-mode-level';},
+    command: 'AT+CMIC='+ [mode,level].join(','),
+    regexp: /^$/
+  },
+  {
+    regexp: /OK/,
+    onMatch: function() {
+      self.state = 'waiting';
+      taskIsDone();
+    }
+  }
+  ];
+
+  this._serialDevice.enqueue(tasks);
+}
